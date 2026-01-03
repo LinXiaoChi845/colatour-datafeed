74cefbd673cb19d0ec375a79685a08e5eaba7af6
LinXiaoChi845 <cola45tour31@gmail.com>
2026-01-03 11:19:35 +0800

    初始化 DataFeed Project

---
+++ src/main/java/tw/com/colatour/datafeed/DataFeedApplication.java
@@ -0,0 +1,13 @@
+package tw.com.colatour.datafeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/com/colatour/datafeed/DataFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.colatour.datafeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
